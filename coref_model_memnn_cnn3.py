e2f72ca4c73588bd956161736c7d9bb5687e32ba
sktim <lhythu@gmail.com>
2018-01-14 16:45:49 +0800

    print gold starts

--- coref_model_memnn_cnn3.py
+++ coref_model_memnn_cnn3.py
@@ -283,6 +283,7 @@ class CorefModel(object):
     self.tag_outputs = tag_outputs
     self.tag_seq = tag_seq
     self.cluster_ids = cluster_ids
+    self.gold_starts = gold_starts
 
     mention_emb = self.get_mention_emb(flattened_text_emb, text_outputs, mention_starts, mention_ends) # [num_candidates, emb]
     # mention_scores = tf.convert_to_tensor([self.get_mention_prob(tag_prob_transpose, mention_starts[i], mention_ends[i], num_words)
